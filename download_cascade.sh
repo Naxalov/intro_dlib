e0665d4392837a508a8456386fbfd72862189b40
ZARIFMAC <uzbzarif@gmail.com>
2023-10-19 15:05:45 +0500

    download cascade

---
+++ download_cascade.sh
@@ -0,0 +1,4 @@
+# Download haarcascade_frontalface_default.xml
+curl -O https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
+# Download haarcascade_eye.xml
+curl -O https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml
